2d454a1817325c5217f5c14f9aeb8781c9bab0b6
Richard <richardosunmu@gmail.com>
2023-10-07 19:53:55 +0100

    Initial commit

---
+++ gpb2.0/src/api/order/controllers/order.ts
@@ -0,0 +1,47 @@
+/**
+ * order controller
+ */
+
+import { factories } from '@strapi/strapi';
+const stripe = require('stripe')(process.env.STRAPI_ADMIN_TEST_STRIPE_SECRET_KEY);
+
+export default factories.createCoreController('api::order.order', ({strapi}) => ({
+    async create(ctx) {
+        const { email, customerDetails, productInfo } = ctx.request.body;
+        let amount = 0;
+
+        for (const element of productInfo) {
+            const product = await strapi.entityService.findOne('api::product.product', element.productId, {
+                fields: ['price', 'discountPrice'],
+              });
+            if (product.discountPrice) {
+                amount = amount + Number(product.discountPrice);
+            }
+            else{
+                amount = amount + Number(product.price);
+            }
+        }
+
+        // Stripe Logic
+        // ---
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: amount,
+            currency: 'ngn',
+            automatic_payment_methods: {
+              enabled: true,
+            },
+          });
+        // ---
+
+        const order = await strapi.entityService.create('api::order.order', {
+            data: {
+              email,
+              total: amount,
+              customerDetails,
+              productInfo
+            },
+          });
+    
+        return {clientSecret: paymentIntent.client_secret};
+      }
+}));
---
+++ gpb2.0/types/generated/components.d.ts
@@ -0,0 +1,45 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface CustomerCustomerDetails extends Schema.Component {
+  collectionName: 'components_customer_customer_details';
+  info: {
+    displayName: 'customerDetails';
+  };
+  attributes: {
+    firstName: Attribute.String;
+    lastName: Attribute.String;
+    companyName: Attribute.String;
+    country: Attribute.String;
+    zipcode: Attribute.String;
+    state: Attribute.String;
+    city: Attribute.String;
+    address: Attribute.Text;
+    phoneNumber: Attribute.String;
+  };
+}
+
+export interface CustomerProducts extends Schema.Component {
+  collectionName: 'components_customer_products';
+  info: {
+    displayName: 'products';
+    description: '';
+  };
+  attributes: {
+    product_id: Attribute.Relation<
+      'customer.products',
+      'oneToOne',
+      'api::order.order'
+    >;
+    colour: Attribute.String & Attribute.DefaultTo<'Satisfactory'>;
+    exclusivity: Attribute.Boolean & Attribute.DefaultTo<false>;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'customer.customer-details': CustomerCustomerDetails;
+      'customer.products': CustomerProducts;
+    }
+  }
+}
