18a4b66a11f1d640f884fe29b91c4c3bcae97cf9
Richard <richardosunmu@gmail.com>
2023-12-06 11:19:59 +0100

    fix: populate image field in the request body

--- gpb2.0/src/api/client/controllers/client.ts
+++ gpb2.0/src/api/client/controllers/client.ts
@@ -4,4 +4,16 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::client.client');
+export default factories.createCoreController('api::client.client', ({strapi}) => ({
+    async find(ctx) {
+    
+        const { meta } = await super.find(ctx);
+        const data = await strapi.entityService.findMany('api::client.client', {
+            populate: 'images',
+          });
+    
+        meta.date = Date.now()
+    
+        return { data, meta };
+      },
+}));
