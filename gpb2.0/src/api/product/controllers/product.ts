717d66b9ce11ddba417e28caeb0574c391cf6788
daudujohn <daudujoe@gmail.com>
2023-12-15 06:09:26 +0100

    fix: add exclusivitity based filter

--- gpb2.0/src/api/product/controllers/product.ts
+++ gpb2.0/src/api/product/controllers/product.ts
@@ -2,27 +2,34 @@
  * product controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::product.product', ({strapi}) => ({
+export default factories.createCoreController(
+  "api::product.product",
+  ({ strapi }) => ({
     async find(ctx) {
-    
-        const { meta } = await super.find(ctx);
-        const data = await strapi.entityService.findMany('api::product.product', {
-            populate: ['productImage', 'backgroundColourOptions'],
-          });
-    
-        meta.date = Date.now()
-    
-        return { data, meta };
-      },
-    
+      const { meta } = await super.find(ctx);
+      const data = await strapi.entityService.findMany("api::product.product", {
+        filters: { exclusivity: false },
+        populate: ["productImage", "backgroundColourOptions"],
+      });
+
+      meta.date = Date.now();
+
+      return { data, meta };
+    },
+
     async findOne(ctx) {
-        let { id } = ctx.request.params;
-        const data = await strapi.entityService.findOne('api::product.product', id, {
-            populate: ['productImage', 'backgroundColourOptions'],
-          });
-        
-        return data;
-    }
-}));
+      let { id } = ctx.request.params;
+      const data = await strapi.entityService.findOne(
+        "api::product.product",
+        id,
+        {
+          populate: ["productImage", "backgroundColourOptions"],
+        }
+      );
+
+      return data;
+    },
+  })
+);
