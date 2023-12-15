9f421a6f6800ad51c4ae7dc231e053a2bd444045
daudujohn <daudujoe@gmail.com>
2023-12-15 06:28:05 +0100

    fix: populate individual client image

--- gpb2.0/src/api/client/controllers/client.ts
+++ gpb2.0/src/api/client/controllers/client.ts
@@ -2,18 +2,32 @@
  * client controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::client.client', ({strapi}) => ({
+export default factories.createCoreController(
+  "api::client.client",
+  ({ strapi }) => ({
     async find(ctx) {
-    
-        const { meta } = await super.find(ctx);
-        const data = await strapi.entityService.findMany('api::client.client', {
-            populate: 'images',
-          });
-    
-        meta.date = Date.now()
-    
-        return { data, meta };
-      },
-}));
+      const { meta } = await super.find(ctx);
+      const data = await strapi.entityService.findMany("api::client.client", {
+        populate: "images",
+      });
+
+      meta.date = Date.now();
+
+      return { data, meta };
+    },
+    async findOne(ctx) {
+      let { id } = ctx.request.params;
+      const data = await strapi.entityService.findOne(
+        "api::client.client",
+        id,
+        {
+          populate: "images",
+        }
+      );
+
+      return data;
+    },
+  })
+);
